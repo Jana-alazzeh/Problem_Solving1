edb23b9c1d856d6dd105de185fab2b667114b0a6
Jana Alazzeh <jana.alazzeh4931@gmail.com>
2026-01-15 08:56:48 +0000

    ,

---
+++ MyApp/Program.cs
@@ -0,0 +1,35 @@
+﻿/* task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7 */
+using System;
+
+// استدعاء الدالة مباشرة
+double result = MyOps.basicOp('+', 42, 7);
+double r = MyOps.basicOp('-', 42, 7);
+double re = MyOps.basicOp('*', 42, 7);
+double res = MyOps.basicOp('/', 42, 7);
+
+Console.WriteLine($"The result is: {result}");
+
+// تغيير اسم الكلاس لتجنب الخطأ
+public static class MyOps 
+{
+    public static double basicOp(char op, double val1, double val2)
+    {
+        switch (op)
+        {
+            case '+': return val1 + val2;
+            case '-': return val1 - val2;
+            case '*': return val1 * val2;
+            case '/': return val1 / val2;
+            default: return 0;
+        }
+    }
+}
